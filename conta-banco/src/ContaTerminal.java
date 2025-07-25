7ef2d47c999df02a11d088fd787728bc1ab5c5e1
Jonas <Neocto@proton.me>
2025-07-24 22:58:30 -0300

    meu projeto conta banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -2,15 +2,19 @@ import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        var scanner = new Scanner(System.in);
+    try (Scanner scanner = new Scanner(System.in)){
     System.out.println("Por favor, digite seu nome.");
         var name = scanner.nextLine(); //scanner.next causava um erro para palavras compostas
     System.out.println("Digite a sua agência.");
-        var agence = scanner.next();    
+        var agence = scanner.next(); 
+        scanner.nextLine(); 
     System.out.println("Digite o número da sua conta.");
         var account = scanner.nextInt();
+        scanner.nextLine();
     System.out.println("Digite o valor do seu saldo.");
         var balance = scanner.nextFloat();
+        scanner.nextLine(); // mesmo sem o nextLine por ser a última linha, não teria problema
     System.out.println("Olá "  +  name + ", obrigado por criar uma conta em nosso banco, sua agência é " + agence + ", conta " + account + " e seu saldo " + balance + " já está disponível para saque.");
     }
+}
 }
